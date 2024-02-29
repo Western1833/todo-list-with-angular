38996044cc1cce82144591509efa0df8aed41087
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-02-29 06:36:42 +0200

    separated in different components and finished css

--- todo-project/src/app/app.module.ts
+++ todo-project/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
+import { InputFieldComponent } from './input-field/input-field.component';
+import { ResultContainerComponent } from './result-container/result-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    ContainerComponent,
+    InputFieldComponent,
+    ResultContainerComponent
   ],
   imports: [
     BrowserModule
