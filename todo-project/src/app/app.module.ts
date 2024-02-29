d198246852427c2c2fbd1434bade753274ef8aa2
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-02-29 07:20:36 +0200

    Added FormsModule and 'add' button correctly takes input data

--- todo-project/src/app/app.module.ts
+++ todo-project/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
@@ -14,7 +15,8 @@ import { ResultContainerComponent } from './result-container/result-container.co
     ResultContainerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- todo-project/src/app/input-field/input-field.component.ts
+++ todo-project/src/app/input-field/input-field.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./input-field.component.css']
 })
 export class InputFieldComponent {
+  newItem: string = '';
 
+  inputDataHandler() {
+    console.log(this.newItem);
+  }
 }
