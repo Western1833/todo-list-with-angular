a01c2d15a3efa078964a0b51a6e7bb8dc11f1b57
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-02-29 08:56:43 +0200

    Added the ul into separate component

--- todo-project/src/app/app.module.ts
+++ todo-project/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
 import { InputFieldComponent } from './input-field/input-field.component';
 import { ResultContainerComponent } from './result-container/result-container.component';
+import { ListItemsComponent } from './list-items/list-items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
     InputFieldComponent,
-    ResultContainerComponent
+    ResultContainerComponent,
+    ListItemsComponent
   ],
   imports: [
     BrowserModule,
